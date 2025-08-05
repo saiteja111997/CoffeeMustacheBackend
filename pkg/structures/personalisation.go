c406960c0091db07a13841e7f04ca547c16822dc
saiteja111997 <tej111997@gmail.com>
2025-08-05 17:08:58 +0530

    Add is_customizable field to ItemObject and update personalized data queries

--- pkg/structures/personalisation.go
+++ pkg/structures/personalisation.go
@@ -1,11 +1,12 @@
 package structures
 
 type ItemObject struct {
-	ID       uint    `json:"id"`
-	CafeID   uint    `json:"cafe_id"`
-	ImageURL string  `json:"image_url"`
-	Name     string  `json:"name"`
-	Price    float64 `json:"price"`
+	ID             uint    `json:"id"`
+	CafeID         uint    `json:"cafe_id"`
+	ImageURL       string  `json:"image_url"`
+	Name           string  `json:"name"`
+	Price          float64 `json:"price"`
+	IsCustomizable bool    `json:"is_customizable"`
 }
 
 type PersonalisedDataResponse struct {
